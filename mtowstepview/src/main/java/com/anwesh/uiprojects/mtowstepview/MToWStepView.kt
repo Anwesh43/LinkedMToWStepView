46ed4a9e4eeb31a8d517b9cf2deb42b5c931b755
Anwesh43 <anweshthecool0@gmail.com>
2018-12-11 10:30:20 +0530

    adding libraries and defining constants

--- mtowstepview/src/main/java/com/anwesh/uiprojects/mtowstepview/MToWStepView.kt
+++ mtowstepview/src/main/java/com/anwesh/uiprojects/mtowstepview/MToWStepView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.mtowstepview
 /**
  * Created by anweshmishra on 11/12/18.
  */
+
+import android.app.Activity
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val color : Int = Color.parseColor("#6A1B9A")
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.7f
